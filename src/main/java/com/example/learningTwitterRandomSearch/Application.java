47a66e3c392179f64145890b3fceed98c119bbcd
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-08-02 16:14:09 +0900

    first commit.

---
+++ src/main/java/com/example/learningTwitterRandomSearch/Application.java
@@ -0,0 +1,26 @@
+package com.example.learningTwitterRandomSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args)) {
+			Application app = ctx.getBean(Application.class);
+			app.run();
+		} catch (Exception e){
+			System.out.println("Application Error!");
+			e.printStackTrace();
+		}
+	}
+
+	private void run() throws Exception {
+		var greeting = "Hello";
+		var framework = "Spring Boot 2";
+		System.out.println("Application Run!");
+		System.out.println(String.format("%s %s!", greeting, framework));
+	}
+}
